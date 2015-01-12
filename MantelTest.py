2a083edafeee9a84ffa5f0c3fae91eea0582a414
Jon Carr <jwcarr@users.noreply.github.com>
2015-01-12 21:15:34 +0000

    Added LevenshteinDistance() function
    computes the normalized Levenshtein distance between two strings

--- MantelTest.py
+++ MantelTest.py
@@ -64,6 +64,24 @@ def MantelTest(distances1, distances2, randomizations):
 ############ END OF MonteCarlo() ############
 
 
+# LevenshteinDistance()
+#   Takes two stirngs and returns the normalized Levenshtein distance
+
+def LevenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1,s2 = s2,s1
+    distances = range(len(s1) + 1)
+    for index2, char2 in enumerate(s2):
+        newDistances = [index2 + 1]
+        for index1, char1 in enumerate(s1):
+            if char1 == char2:
+                newDistances.append(distances[index1])
+            else:
+                newDistances.append(1 + min((distances[index1], distances[index1+1], newDistances[-1])))
+        distances = newDistances
+    return float(distances[-1])/max(len(s1), len(s2))
+
+
 # ShuffleDistances()
 #   Takes a list of pairwise distances, converts it to a distance matrix,
 #   shuffles the matrix, and returns the upper triangle as a vector.
